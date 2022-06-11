e2295aa9c98bff510deef6a7227a6fc725695eb3
Your Name <you@example.com>
2022-06-12 01:34:10 +0900

    Refactoring minicc_gen

--- minicc_gen/src/lib.rs
+++ minicc_gen/src/lib.rs
@@ -48,80 +48,89 @@ impl<'a> Gen<'a> {
 
     fn gen(&mut self, node: &ast::Ast) {
         match &node.kind {
-            ast::AstKind::CompoundStmt(n) => {
-                for i in &n.items {
-                    self.gen(i);
-                }
+            ast::AstKind::CompoundStmt(n) => self.compound_stmt(n, node.loc),
+            ast::AstKind::Decl(n) => self.decl(n, node.loc),
+            ast::AstKind::Ref(n) => self.ref_(n, node.loc),
+            ast::AstKind::IntLit(n) => self.int_lit(n, node.loc),
+            ast::AstKind::UnOp(n) => self.un_op(n, node.loc),
+            ast::AstKind::BinOp(n) => self.bin_op(n, node.loc),
+        }
+    }
+
+    fn compound_stmt(&mut self, node: &ast::CompoundStmt, _loc: usize) {
+        for i in &node.items {
+            self.gen(i);
+        }
+    }
+
+    fn decl(&mut self, node: &ast::Decl, _loc: usize) {
+        self.offset += 4;
+        self.vars.insert(node.ident.clone(), self.offset);
+    }
+
+    fn ref_(&mut self, node: &ast::Ref, loc: usize) {
+        if let Some(offset) = self.vars.get(&node.ident) {
+            o!(self.f, "	mov	-{}(%ebp), %eax", offset);
+        } else {
+            self.err(loc, &format!("cannot find value `{}`", node.ident));
+        }
+    }
+
+    fn int_lit(&mut self, node: &ast::IntLit, _loc: usize) {
+        o!(self.f, "	mov	${}, %eax", node.val);
+    }
+
+    fn un_op(&mut self, node: &ast::UnOp, _loc: usize) {
+        self.gen(&node.expr);
+
+        match node.op {
+            ast::OpUn::Neg => {
+                o!(self.f, "	neg	%eax");
             }
-            ast::AstKind::Decl(n) => {
-                self.offset += 4;
-                self.vars.insert(n.ident.clone(), self.offset);
-            }
-            ast::AstKind::Ref(n) => {
-                if let Some(offset) = self.vars.get(&n.ident) {
-                    o!(self.f, "	mov	-{}(%ebp), %eax", offset);
+        }
+    }
+
+    fn bin_op(&mut self, node: &ast::BinOp, loc: usize) {
+        if node.op == ast::OpBin::Asign {
+            self.gen(&node.rhs);
+
+            if let ast::AstKind::Ref(l) = &node.lhs.kind {
+                if let Some(offset) = self.vars.get(&l.ident) {
+                    o!(self.f, "	mov	%eax, -{}(%ebp)", offset)
                 } else {
-                    self.err(
-                        node.loc,
-                        &format!("cannot find value `{}`", n.ident),
-                    );
+                    self.err(loc, &format!("cannot find value `{}`", l.ident))
                 }
+            } else {
+                self.err(loc, "expression is not assignable");
             }
-            ast::AstKind::IntLit(n) => {
-                o!(self.f, "	mov	${}, %eax", n.val);
+            return;
+        }
+
+        self.gen(&node.rhs);
+        o!(self.f, "	push	%eax");
+        self.gen(&node.lhs);
+
+        o!(self.f, "	pop	%ecx");
+        match node.op {
+            ast::OpBin::Add => {
+                o!(self.f, "	add	%ecx, %eax");
             }
-            ast::AstKind::UnOp(n) => {
-                self.gen(&n.expr);
-
-                match n.op {
-                    ast::OpUn::Neg => {
-                        o!(self.f, "	neg	%eax");
-                    }
-                }
+            ast::OpBin::Sub => {
+                o!(self.f, "	sub	%ecx, %eax");
             }
-            ast::AstKind::BinOp(n) => {
-                if n.op == ast::OpBin::Asign {
-                    self.gen(&n.rhs);
-
-                    if let ast::AstKind::Ref(l) = &n.lhs.kind {
-                        if let Some(offset) = self.vars.get(&l.ident) {
-                            o!(self.f, "	mov	%eax, -{}(%ebp)", offset)
-                        } else {
-                            self.err(node.loc, "cannot find value `{}`")
-                        }
-                    } else {
-                        self.err(node.loc, "expression is not assignable");
-                    }
-                    return;
-                }
-
-                self.gen(&n.rhs);
-                o!(self.f, "	push	%eax");
-                self.gen(&n.lhs);
-
-                o!(self.f, "	pop	%ecx");
-                match n.op {
-                    ast::OpBin::Add => {
-                        o!(self.f, "	add	%ecx, %eax");
-                    }
-                    ast::OpBin::Sub => {
-                        o!(self.f, "	sub	%ecx, %eax");
-                    }
-                    ast::OpBin::Mul => {
-                        o!(self.f, "	imul	%ecx, %eax");
-                    }
-                    ast::OpBin::Div => {
-                        o!(self.f, "	cltd");
-                        o!(self.f, "	idiv	%ecx");
-                    }
-                    ast::OpBin::Mod => {
-                        o!(self.f, "	cltd");
-                        o!(self.f, "	idiv	%ecx");
-                        o!(self.f, "	mov	%edx, %eax");
-                    }
-                    _ => unreachable!(),
-                }
+            ast::OpBin::Mul => {
+                o!(self.f, "	imul	%ecx, %eax");
             }
+            ast::OpBin::Div => {
+                o!(self.f, "	cltd");
+                o!(self.f, "	idiv	%ecx");
+            }
+            ast::OpBin::Mod => {
+                o!(self.f, "	cltd");
+                o!(self.f, "	idiv	%ecx");
+                o!(self.f, "	mov	%edx, %eax");
+            }
+            _ => unreachable!(),
         }
     }
 
